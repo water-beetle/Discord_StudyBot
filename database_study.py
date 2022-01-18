60a059ff2101878efa215ad47b6d3b8ca181524b
water-beetle <60806919+water-beetle@users.noreply.github.com>
2022-01-18 09:36:16 +0900

    Add files via upload

---
+++ database_study.py
@@ -0,0 +1,140 @@
+import pymysql
+from datetime import datetime
+
+
+class DBupdater:
+    def __init__(self):
+        """생성자 : MariaDB 연결 및 딕셔너리 생성"""
+        self.conn = pymysql.connect(host='localhost', user='root', password='dongdong', db='discord_bot',
+                                    charset='utf8')
+
+        with self.conn.cursor() as curs:
+            sql = """
+            CREATE TABLE IF NOT EXISTS attend_date (
+                `user_name` VARCHAR(20) NOT NULL COLLATE 'utf8mb3_general_ci',
+                `today_study_time` TIME NULL DEFAULT NULL,
+                `today_date` DATETIME NOT NULL,
+                `is_achieve` TINYINT(2) NULL DEFAULT NULL,
+                PRIMARY KEY (`user_name`, `today_date`) USING BTREE
+            )
+            COLLATE='utf8mb3_general_ci'
+            ENGINE=InnoDB;
+            """
+            curs.execute(sql)
+            sql = """
+            CREATE TABLE IF NOT EXISTS attend_info (
+                `user_name` VARCHAR(20) NOT NULL COLLATE 'utf8mb3_general_ci',
+                `total_study_time` TIME NULL DEFAULT NULL,
+                `target_time` TIME NULL DEFAULT NULL,
+                PRIMARY KEY (`user_name`) USING BTREE
+            )
+            COLLATE='utf8mb3_general_ci'
+            ENGINE=InnoDB;
+            """
+
+        self.conn.commit()
+
+    def __del__(self):
+        """소멸자 : MariaDB 연결 해제"""
+        self.conn.close()
+
+    # !등록
+    def update(self, name) -> None:
+        with self.conn.cursor() as curs:
+            sql = f"""INSERT INTO attend_info(user_name, total_study_time, target_time) VALUES('{name}', 
+            '00:00:00',  '00:00:00') """
+            curs.execute(sql)
+        self.conn.commit()
+
+    # 등록된 사용자인지 검사
+    def is_admit(self, name)->bool:
+        with self.conn.cursor() as curs:
+            sql = f"""
+            SELECT user_name FROM attend_info
+            WHERE user_name="{name}"
+            """
+            curs.execute(sql)
+            result = curs.fetchone()
+        return result is None
+
+    # attend_info에 [이름, 날짜]가 저장되어있는지 확인
+    def is_admit_today(self, name, date)->bool:
+        with self.conn.cursor() as curs:
+            sql = f"""
+            SELECT * FROM attend_date
+            WHERE user_name='{name}' AND today_date='{date}'
+            """
+            curs.execute(sql)
+            result = curs.fetchone()
+        return result is None
+
+    # !목표시간 설정
+    def update_2(self, name, target) -> None:
+        with self.conn.cursor() as curs:
+            sql = f'''UPDATE attend_info SET target_time = '{target}' WHERE user_name = '{name}' '''
+            curs.execute(sql)
+        self.conn.commit()
+
+    # !종료
+    # 현재날짜, 하루 공부시간, 목표시간 < 하루 공부시간이면 is_achieve 1
+    def update_3(self, name, today_study_time):
+        with self.conn.cursor() as curs:
+            sql = f'''
+            SELECT target_time FROM attend_info WHERE user_name = '{name}' '''
+            curs.execute(sql)
+            target_time = curs.fetchone()[0]
+            is_achieve = 0
+            today = datetime.today().strftime("%Y-%m-%d")
+
+            if(today_study_time > target_time):
+                is_achieve = 1
+            print(target_time)
+            sql = f"""INSERT INTO attend_date(user_name, today_study_time, today_date, is_achieve) VALUES('{name}', 
+            '{today_study_time}',  '{today}', {is_achieve})"""
+            curs.execute(sql)
+        self.conn.commit()
+
+    # !종료
+    # 값 수정
+    def update_4(self, name, today_study_time):
+        with self.conn.cursor() as curs:
+            sql = f'''
+            SELECT target_time FROM attend_info WHERE user_name = '{name}' '''
+            curs.execute(sql)
+            target_time = curs.fetchone()[0]
+            is_achieve = 0
+            today = datetime.today().strftime("%Y-%m-%d")
+
+            if(today_study_time > target_time):
+                is_achieve = 1
+
+            sql = f"""
+            UPDATE attend_date SET today_study_time='{today_study_time}', is_achieve = {is_achieve} WHERE user_name='{name}' AND today_date = '{today}';
+            """
+            curs.execute(sql)
+        self.conn.commit()
+
+    # total_study_time 업데이트
+    def update_5(self, name, study_time):
+        with self.conn.cursor() as curs:
+            sql = f'''
+            SELECT total_study_time FROM attend_info WHERE user_name ='{name}'
+            '''
+            curs.execute(sql)
+            total_study_time = curs.fetchone()[0] + study_time
+            sql = f'''
+            UPDATE attend_info SET total_study_time='{total_study_time}' WHERE user_name='{name}'
+            '''
+            curs.execute(sql)
+        self.conn.commit()
+
+    def get_info(self, name)->str:
+        with self.conn.cursor() as curs:
+            sql = f'''
+            SELECT today_study_time FROM attend_date WHERE user_name='{name}' and today_date='{datetime.today().strftime("%Y-%m-%d")}'
+            '''
+            curs.execute(sql)
+            study_time = curs.fetchone()[0]
+        return study_time
+
+
---
+++ main.py
@@ -0,0 +1,185 @@
+import asyncio
+import discord
+import datetime
+from database_study import DBupdater
+from collections import defaultdict
+from discord.ext import commands
+
+app = commands.Bot(command_prefix='!')
+db = DBupdater()
+# 상수 모음
+ATTEND_TIME = '09:00'
+
+# 전역변수 모음
+today_study = {}  # 오늘 공부에 참여한 인원들의 하루 공부시간 저장하는 변수 {이름 : [시작시간, 종료시간, 시작시간, 종료시간...]}
+today_rest_time = defaultdict(datetime.timedelta)  # 오늘 휴식한 인원들의 휴식시간 {이름 : 휴식시간}
+today_attend = []  # 오늘 출석 여부 변수
+embed = discord.Embed(title="출석정보", colour=discord.Colour.purple())  # 출석 정보 출력
+count = defaultdict(datetime.timedelta)
+today_study_time = defaultdict(datetime.timedelta)
+
+
+@app.event
+async def on_ready():
+    print(f'{app.user.name} 연결성공')
+    await app.change_presence(status=discord.Status.online, activity=None)
+
+
+##############
+###기타 함수###
+##############
+def strfdelta(tdelta, fmt):
+    d = {}
+    d["hours"], rem = divmod(tdelta.seconds, 3600)
+    d["minutes"], d["seconds"] = divmod(rem, 60)
+    return fmt.format(**d)
+
+
+######################
+### 명령어 처리 함수 ###
+######################
+
+# db에 사용자 등록
+@app.command()
+async def 등록(ctx):
+    # 등록된 사용자가 아닐 경우
+    if db.is_admit(ctx.author.name):
+        db.update(ctx.author.name)
+        await ctx.send(f"[{ctx.author.name}] - 등록 완료")
+    # 등록된 사용자인 경우
+    else:
+        await ctx.send(f"[{ctx.author.name}] - 이미 동록된 사용자입니다")
+
+
+# 목표시간 설정
+@app.command()
+async def 목표시간(ctx, time):
+    # 등록된 사용자가 아닐 경우
+    if db.is_admit(ctx.author.name):
+        await ctx.send(f"'!등록'을 먼저 입력해주세요")
+    # 등록된 사용자인 경우
+    else:
+        db.update_2(ctx.author.name, datetime.timedelta(hours=int(time)))
+        await ctx.send(f"[{ctx.author.name}] - 목표시간 {time}시간 설정")
+
+
+# [attend_time] 변경 필요
+@app.command()
+async def 출석(ctx):
+    if db.is_admit(ctx.author.name):
+        await ctx.send(f"'!등록'을 먼저 입력해주세요")
+        return;
+
+    if (ctx.author.name in today_attend):
+        await ctx.send("이미 출석했습니다")
+    else:
+        attend_time = datetime.datetime.now().strftime("%H:%M")
+        # 지각인 경우
+        if (attend_time > ATTEND_TIME):
+            embed.add_field(name=ctx.author.name, value=attend_time + '\n\n:red_square: 지각')
+        # 지각이 아닌 경우
+        else:
+            embed.add_field(name=ctx.author.name, value=attend_time + '\n\n:green_square: 출석')
+
+        today_attend.append(ctx.author.name)
+        today_study[ctx.author.name] = []
+        await ctx.send(embed=embed)
+
+
+@app.command()
+async def 시작(ctx):
+    if db.is_admit(ctx.author.name):
+        await ctx.send(f"'!등록'을 먼저 입력해주세요")
+        return;
+    # 출석체크 여부
+    if ctx.author.name in today_attend:
+        today_study[ctx.author.name].append(datetime.datetime.now())
+        await ctx.send(f"[{ctx.author.name}] - {today_study[ctx.author.name][-1].strftime('%H:%M')} 시작")
+    else:
+        await ctx.send("출석체크 부탁드립니다")
+
+
+
+@app.command()
+async def 휴식(ctx):
+    if db.is_admit(ctx.author.name):
+        await ctx.send(f"'!등록'을 먼저 입력해주세요")
+        return;
+
+    # 출석체크 여부
+    if ctx.author.name not in today_attend:
+        await ctx.send("출석체크 부탁드립니다")
+    # 시작여부 확인
+    elif len(today_study[ctx.author.name]) % 2 == 0:
+        await ctx.send("!시작을 먼저 입력해주세요")
+    else:
+        # 공부 종료시간 입력
+        today_study[ctx.author.name].append(datetime.datetime.now())
+
+        # wait_for 함수 handler
+        def check(message):
+            return message.content == "!휴식끝" and message.author == ctx.author
+
+        # 기본 10분 휴식, 10분지나면 mention 후 다시 10분 휴식
+        async def wait_user():
+            try:
+                rest_time = (datetime.datetime.now() + datetime.timedelta(minutes=10))
+                await ctx.send(f"[{ctx.author.name}] - 10분 휴식\n {rest_time.strftime('%H:%M')}분 까지 '!휴식끝'을 입력해주세요")
+                msg = await app.wait_for('message', timeout=600.0, check=check)
+            except asyncio.TimeoutError:
+                count[ctx.author.name] += datetime.timedelta(minutes=10)
+                await ctx.send(f'{ctx.author.mention} 10분 지났습니다...')
+                await wait_user()
+            else:
+                total_rest_time = datetime.datetime.now() + datetime.timedelta(minutes=10) - rest_time + count[ctx.author.name]
+                # today_rest_time에 사용자 휴식시간을 더해줌
+                today_rest_time[ctx.author.name] += total_rest_time
+                await ctx.send(
+                    f'[{ctx.author.name}] - 휴식 끝 \n 휴식시간 - {strfdelta(total_rest_time, "{minutes}분{seconds}초")}')
+                # 공부 시작시간 입력
+                today_study[ctx.author.name].append(datetime.datetime.now())
+                #휴식 count 초기화
+                count[ctx.author.name] = datetime.timedelta()
+
+        await wait_user()
+
+
+@app.command()
+async def 종료(ctx):
+    if db.is_admit(ctx.author.name):
+        await ctx.send(f"'!등록'을 먼저 입력해주세요")
+        return;
+    # 출석체크 확인
+    if ctx.author.name not in today_attend:
+        await ctx.send("출석체크 부탁드립니다")
+    # 시작여부 확인
+    elif len(today_study[ctx.author.name]) % 2 == 0:
+        await ctx.send("!시작을 먼저 입력해주세요")
+    # db 업데이트 및 공부시간 출력
+    else:
+        today_study[ctx.author.name].append(datetime.datetime.now())
+        for i in range(0, len(today_study[ctx.author.name]), 2):
+            start_time = today_study[ctx.author.name][i]
+            end_time = today_study[ctx.author.name][i + 1]
+            print(start_time, end_time)
+            today_study_time[ctx.author.name] += end_time - start_time
+
+        await ctx.send(
+            f"[{ctx.author.name}] - 공부 끝!\n:alarm_clock:  {today_study[ctx.author.name][0].strftime('%H:%M:%S')} ~ "
+            f"{datetime.datetime.now().strftime('%H:%M:%S')}")
+        # 오늘 처음 공부 종료를 할 경우
+        if db.is_admit_today(ctx.author.name, datetime.date.today()):
+            db.update_3(ctx.author.name, today_study_time[ctx.author.name])
+        # db에서 오늘 공부한 시간을 가져옴
+        else:
+            db.update_4(ctx.author.name, db.get_info(ctx.author.name) + today_study_time[ctx.author.name])
+
+        # db의 total_study_time 업데이트
+        db.update_5(ctx.author.name, today_study_time[ctx.author.name])
+
+        await ctx.send(
+            f':book: 공부 시간 : {strfdelta(today_study_time[ctx.author.name] - today_rest_time[ctx.author.name], "{hours}시간{minutes}분{seconds}초")}')
+        today_rest_time[ctx.author.name] = datetime.timedelta()
+
+
+app.run('OTMxNDQzMjEyNDU4MDIwOTM0.YeEgFw.P75shc3mWT5eubQV4PK7OhLjR9M')
