9813178ced93d76d7309e88ba2328f0f35f59c82
Brown <water_beetle@naver.com>
2022-01-21 16:06:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -280,7 +280,7 @@ async def 랭킹(ctx):
 @app.command()
 async def 기록(ctx):
     week_table_class = week_table(ctx.author.name)
-    
+    week_table_class.check_file_exists()
 
     with BytesIO as image_binary:
         week_table_class.im.save(image_binary, "png")
