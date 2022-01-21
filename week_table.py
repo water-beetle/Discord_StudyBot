ebac4a957edc43c670e289f4b623a19c803e5604
Brown <water_beetle@naver.com>
2022-01-21 10:35:35 +0900

    font

--- week_table.py
+++ week_table.py
@@ -4,6 +4,11 @@ from io import BytesIO
 
 
 def get_week_table():
+
+    font = "my_font.ttf"
+    imageFont = ImageFont.truetype(font, 50)
+
+
     date_im = []
     menu_im = []
     date_colors = [(77, 137, 99), (105, 165, 131), (225, 179, 120), (224, 204, 151), (236, 121, 154), (159, 2, 81),
@@ -12,6 +17,7 @@ def get_week_table():
 
     im = Image.new(mode="RGB", size=(960, 360), color=(255, 255, 255))
     line = Image.new("RGB", (120, 90), (230, 230, 230))
+    d = ImageDraw.Draw(im)
 
     # 날짜 이미지 추가
     for date in range(0, 7):
@@ -30,6 +36,8 @@ def get_week_table():
         for col in range(1, 8):
             if (row + col) % 2 == 1:
                 im.paste(line, (120 * col, 90 * row))
+
+    d.text((0, 0), "테스트", font = font, fill = (0, 0, 0))
 
     im.show()
     return im
